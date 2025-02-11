eaf25ea97972b2fa019b655b41abd023184da366
iloveplant18 <degradant31@gmail.com>
2025-02-11 22:17:31 +0300

    add locations mechanic doc, ideas doc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,7 +48,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/iloveplant18/game-doc/',
+            'https://github.com/iloveplant18/game-doc/tree/main/',
         },
         blog: false,
         theme: {
